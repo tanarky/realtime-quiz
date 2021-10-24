0723aa30dd189936eabcb86c91d4c49956661d14
Satoshi Tanaka <tanarky@gmail.com>
2021-10-24 09:24:27 +0900

    use static dir application.js

--- app.rb
+++ app.rb
@@ -5,11 +5,5 @@ module ChatDemo
     get "/" do
       erb :"index.html"
     end
-
-    get "/assets/js/application.js" do
-      content_type :js
-      @scheme = ENV['RACK_ENV'] == "production" ? "wss://" : "ws://"
-      erb :"application.js"
-    end
   end
 end
